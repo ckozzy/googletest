98a8ab0ab9c0158ddd11096c197776a0662071e3
C Kelly Osborn <kelly@shadetree.biz>
2016-02-18 14:50:14 -0700

    Added files to this repository after running cmake

---
+++ googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/google/googletest
+# Build directory: D:/google/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
